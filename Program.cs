3fe905a6417ac249157efc9d14892773f73f8a94
johnmaster <juanpuhl404@gmail.com>
2022-11-17 08:18:16 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace ejerc_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int DIAS=5;
+            const int SEMANA=4;
+            
+            int[ , ] arregloTemp = new int[SEMANA, DIAS];
+
+        
+            float  temperaturaMinima =arregloTemp[0 , 0], temperaturaMaxima=arregloTemp[0 , 0];
+            int diaMax=arregloTemp[0 , 0], diaMin=300;
+            
+
+            
+
+            Console.WriteLine(". . . REGISTRO DE TEMPERATURA . . . ");
+
+            // registra las temperaturas ingresadas por el usuario
+           for(int i=0; i<SEMANA; i++){
+
+                for(int j=0; j< DIAS; j++ ){
+
+                    Console.WriteLine("Ingrese temperatura, dia: {0} Semana: {1}", j+1 , i+1);
+                    arregloTemp[i,j]=Convert.ToInt32(Console.ReadLine());
+
+                }
+            }
+            
+
+            for(int i=0; i< SEMANA; i++){
+                for(int j=0; j< DIAS; j++){
+                    if(diaMax < arregloTemp[i , j]){
+                        diaMax=arregloTemp[i , j];
+                    }
+                    if(diaMin > arregloTemp[i , j]){
+                        diaMin=arregloTemp[i , j];
+                    }
+
+                }
+
+                Console.Clear();
+                Console.WriteLine("LA TEMPERATURA MAXIMA FUE: " + diaMax);
+                Console.WriteLine("LA TEMPERATURA MINIMA FUE: " + diaMin);
+
+
+            }
+
+
+           }
+
+           
+        }
+}
+
